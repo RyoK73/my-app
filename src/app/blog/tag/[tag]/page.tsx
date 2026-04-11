04849852bffafd3ebcd1363e4cb3958de9e99583
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-04-12 01:23:32 +0900

    feat:タグでフィルターされたTagPageを実装
    - 各タグLinkから遷移するページ

--- src/app/blog/tag/[tag]/page.tsx
+++ src/app/blog/tag/[tag]/page.tsx
@@ -1,11 +1,16 @@
 import { getAllPosts } from "@/lib/post";
 import { getPostsByTag } from "@/lib/post";
+import PostListByTag from "@/components/ui/post-list";
 
 const TagPage = async ({ params }: { params: Promise<{ tag: string }> }) => {
     const posts = await getAllPosts();
     const { tag } = await params;
-    const postsByTag = getPostsByTag(tag);
-    return(
-        <main></main>
-    )
+    return (
+        <main>
+            <h1>{tag}の記事一覧</h1>
+            <PostListByTag tag={tag} />
+        </main>
+    );
 };
+
+export default TagPage;
