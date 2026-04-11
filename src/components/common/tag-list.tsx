e9e2c323a1f88a1eded5457ad6433248be0225ee
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-04-12 01:33:53 +0900

    feat:tagLinkからのリンク先ルーティングを修正
    - process.cwd()からにしていたのを"/blog/..."に修正

--- src/components/common/tag-list.tsx
+++ src/components/common/tag-list.tsx
@@ -2,13 +2,12 @@ import Link from "next/link";
 import path from "path";
 // tagsを受け取って、Linkをバッジ表示する。各リンク先は、./@/app/blog/tag/[tag]へ
 const TagList = ({ tags }: { tags: string[] }) => {
-    const tagPagePath = path.join(process.cwd(), "blog/tag");
     return (
         // 横に並べる感じ
         <div>
             {tags.map((tag) => {
                 return (
-                    <Link key={tag} href={path.join(tagPagePath, tag)}>
+                    <Link key={tag} href={path.join("/blog/tag", tag)}>
                         {tag}
                     </Link>
                 );
